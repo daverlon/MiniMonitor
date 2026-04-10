4390b3d434fc807a04d42ff6eb89e7dd1218c1f6
daverlon <davernlon@gmail.com>
2026-04-10 12:10:15 +0800

    Initial Commit

---
+++ MiniMonitor/MiniMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMonitorApp.swift
+//  MiniMonitor
+//
+//  Created by David on 10/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct MiniMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
